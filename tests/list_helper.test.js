2e405771eb3eebcf3b3e07df450bb449c1ab57ed
tmtemetm <teemu.j.maki@aalto.fi>
2024-05-20 18:38:14 +0300

    Tehtävä 4.4

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,10 +1,31 @@
-const { test } = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
+const blogsForTest = require('./blogs_for_test')
 
 test('dummy returns one', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  test('of an empty list is zero', () => {
+    assert.strictEqual(listHelper.totalLikes([]), 0)
+  })
+
+  test('of a single blog equals the likes of the blog', () => {
+    assert.strictEqual(listHelper.totalLikes([blogsForTest[0]]), blogsForTest[0].likes)
+  })
+
+  test('of a list of blogs equals the sum of blogs\' likes', () => {
+    assert.strictEqual(listHelper.totalLikes([
+      blogsForTest[0],
+      blogsForTest[1],
+      blogsForTest[2]
+    ]), blogsForTest[0].likes
+      + blogsForTest[1].likes
+      + blogsForTest[2].likes)
+  })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,11 @@ const dummy = blogs => {
   return 1
 }
 
+const totalLikes = blogs => {
+  return blogs.reduce((likes, blog) => likes + blog.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
