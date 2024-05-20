becd8db8b1e9b3fab2ff412d54aab3cce588d3ad
tmtemetm <teemu.j.maki@aalto.fi>
2024-05-20 03:22:49 +0300

    Refactor blogsRouter

--- index.js
+++ index.js
@@ -5,30 +5,14 @@ const mongoose = require('mongoose')
 
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
