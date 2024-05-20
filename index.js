685e9d4f0124a138af4216456ff4eb5aa1bfbcec
tmtemetm <teemu.j.maki@aalto.fi>
2024-05-20 03:10:43 +0300

    Refactor model

--- index.js
+++ index.js
@@ -5,15 +5,7 @@ const mongoose = require('mongoose')
 
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 mongoose.connect(config.MONGODB_URI)
 
