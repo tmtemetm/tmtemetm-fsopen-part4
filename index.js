f1104153a7c5b9e54d04d44668d1701bba04c6bd
tmtemetm <teemu.j.maki@aalto.fi>
2024-05-20 03:29:14 +0300

    Refactor app

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
+const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const blogsRouter = require('./controllers/blogs')
-
-mongoose.connect(config.MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
